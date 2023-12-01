9d20b68aca593629f94e55c47221c4b7c79caf8f
Sebastian GONZALEZ MORAN <sebi.gonzalezmoran@gmail.com>
2023-12-01 12:06:40 +0100

    Challenge 01

---
+++ day01/index.js
@@ -0,0 +1,34 @@
+const fs = require('node:fs');
+
+const extract = (arr) => {
+   const nbStrMap = new Map([['zero','0'], ['one', '1'], ['two', '2'], ['three', '3'], ['four', '4'], ['five', '5'], ['six', '6'], ['seven', '7'], ['eight', '8'], ['nine', '9']]);
+   return arr.map((s) => s.match(/((zero|one|two|three|four|five|six|seven|eight|nine|[0-9]).*(zero|one|two|three|four|five|six|seven|eight|nine|[0-9]))|(zero|one|two|three|four|five|six|seven|eight|nine|[0-9])/))
+      .reduce((acc, v) => acc + Number(v[2] ? (nbStrMap.get(v[2]) || v[2]) + (nbStrMap.get(v[3]) || v[3]) : (nbStrMap.get(v[4]) || v[4]).repeat(2)), 0)
+}
+
+const formatToArray = (input) => {
+   return input.split('\n');
+}
+
+const readFile = (path, callback) => {
+   fs.readFile(path, 'utf8', (err, data) => {
+     if (err) {
+       console.error('Error:', err);
+       return;
+     }
+     callback(data);
+   });
+}
+
+const run = (path) => {
+   const callback = (data) => {
+      console.time();
+      console.log(`Response: ${extract(formatToArray(data))}`);
+      console.timeEnd();
+   };
+   readFile(path, callback);
+}
+
+run('./one/sample_silver.txt');
+run('./one/sample_gold.txt');
+run('./one/input.txt');
