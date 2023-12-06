854291a71b24e5d47c248708d878bbc8493b3972
Gonzalez Moran Sebastián <19952324+sebastian-gonzalezmoran@users.noreply.github.com>
2023-12-06 14:39:52 +0100

    challenge 06

---
+++ day06/index.js
@@ -0,0 +1,75 @@
+const fs = require("node:fs");
+const path = require("node:path");
+
+const extractS = (input) => {
+  let mult = 1;
+  const split = input.split("\n");
+
+  const times = split[0].match(/(\d+)/g);
+  const distances = split[1].match(/(\d+)/g);
+
+  for (let i = 0; i < times.length; i++) {
+    const time = Number(times[i]);
+    const distance = Number(distances[i]);
+
+    const minTimeToWait = Math.floor(
+      (-time + Math.sqrt(time ** 2 - 4 * distance)) / -2
+    );
+    const maxTimeToWait = Math.ceil(
+      (-time - Math.sqrt(time ** 2 - 4 * distance)) / -2
+    );
+
+    mult *= maxTimeToWait - minTimeToWait - 1;
+  }
+  return mult;
+};
+
+const extractG = (input) => {
+  let mult = 1;
+  const split = input.split("\n");
+
+  const time = Number(split[0].match(/(\d+)/g).join(""));
+  const distance = Number(split[1].match(/(\d+)/g).join(""));
+
+  const minTimeToWait = Math.floor(
+    (-time + Math.sqrt(time ** 2 - 4 * distance)) / -2
+  );
+  const maxTimeToWait = Math.ceil(
+    (-time - Math.sqrt(time ** 2 - 4 * distance)) / -2
+  );
+
+  return maxTimeToWait - minTimeToWait - 1;
+};
+
+const readFile = (filename, callback) => {
+  fs.readFile(path.join(__dirname, filename), "utf8", (err, data) => {
+    if (err) {
+      console.error("Error:", err);
+      return;
+    }
+    callback(data);
+  });
+};
+
+const runS = (filename) => {
+  const callback = (data) => {
+    console.time();
+    console.log(`Response Silver ${filename}: ${extractS(data)}`);
+    console.timeEnd();
+  };
+  readFile(filename, callback);
+};
+
+const runG = (filename) => {
+  const callback = (data) => {
+    console.time();
+    console.log(`Response Gold ${filename}: ${extractG(data)}`);
+    console.timeEnd();
+  };
+  readFile(filename, callback);
+};
+
+runS("sample.txt");
+runG("sample.txt");
+runS("input.txt");
+runG("input.txt");
