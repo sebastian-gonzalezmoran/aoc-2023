d6e609cdba1ad7c60ad14a108aba876021049590
Sebastian GONZALEZ MORAN <sebi.gonzalezmoran@gmail.com>
2023-12-09 01:21:20 +0100

    Challenge08 F part 2

--- day08/index.js
+++ day08/index.js
@@ -22,7 +22,37 @@ const extractS = (input) => {
 };
 
 const extractG = (input) => {
+  const pathMap = new Map();
 
+  const instructions = input.split('\n', 1)[0].trim();
+  const allPaths = [...input.matchAll(/([A-Z]{3}) = \(([A-Z]{3}), ([A-Z]{3})\)/g)];
+
+  const paths = [...input.matchAll(/([A-Z]{2}A) =/g)].map(v => v[1]);
+
+  allPaths.forEach(path => pathMap.set(path[1], { L: path[2], R: path[3] }));
+  
+  const ways = [];
+  paths.forEach(startNode => {
+    let i = 0;
+    let node = startNode;
+    let instructionsIndex = -1;
+    while (node[2] !== 'Z') {
+      instructionsIndex++;
+      if (instructionsIndex >= instructions.length) instructionsIndex = 0;
+      node = pathMap.get(node)[instructions[instructionsIndex]];
+      i++;
+    }
+    ways.push(i);
+  });
+
+  const gcd = (a, b) => {
+    return b === 0 ? a : gcd(b, a % b);
+  } 
+  let lcm = ways[0];
+  for (let i = 1; i < ways.length; i++) {
+    lcm = (lcm * ways[i]) / gcd(lcm, ways[i]);
+  }
+  return lcm;
 };
 
 const readFile = (filename, callback) => {
@@ -49,6 +79,6 @@ const runG = (filename) => {
 };
 
 runS("sample.txt");
-// runG("sample.txt");
+runG("sample.txt");
 runS("input.txt");
-// runG("input.txt");
+runG("input.txt");
