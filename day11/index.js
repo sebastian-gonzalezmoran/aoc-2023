c92bd8fc7d96636173e8867661678dfc5c7639ea
Gonzalez Moran Sebastián <19952324+sebastian-gonzalezmoran@users.noreply.github.com>
2023-12-12 11:11:26 +0100

    Challenge 11

--- day11/index.js
+++ day11/index.js
@@ -1,26 +1,59 @@
 const fs = require("node:fs");
 const path = require("node:path");
 
-const extractS = (input) => {
-  const universe = input.replace(/(\r\n|\n|\r)/gm, '');
-  const universeWidth = input.indexOf('\n') - 1;
-  const universeExpansion = 2;
+const extract = (input, expansion) => {
+  let sum = 0;
+
+  const universe = input.replace(/(\r\n|\n|\r)/gm, "");
+  const universeWidth = input.indexOf("\n");
+  const universeExpansion = expansion;
 
   const cartX = new Array(universeWidth).fill(universeExpansion);
   const cartY = new Array(universeWidth).fill(universeExpansion);
 
-  const galaxyPositions = [...universe.matchAll(/#/g)].map(v => v.index);
-  galaxyPositions.forEach(pos => {
-    if(cartX === universeExpansion) cartX[pos % universeWidth] = 1;
-    if(cartY === universeExpansion) cartY[pos] = 1; //TODO get line
+  const galaxyPositions = [...universe.matchAll(/#/g)].map((v) => v.index);
+  galaxyPositions.forEach((pos) => {
+    if (cartX[pos % universeWidth] === universeExpansion)
+      cartX[pos % universeWidth] = 1;
+    if (cartY[Math.floor(pos / universeWidth)] === universeExpansion)
+      cartY[Math.floor(pos / universeWidth)] = 1;
   });
-  //TODO increment cartesian map
 
-  //TODO calculate distances with cartesian map
-};
+  let inc = 0;
+  for (let i = 0; i < cartX.length; i++) {
+    if (cartX[i] === universeExpansion) {
+      inc += universeExpansion;
+      cartX[i] = inc;
+    } else {
+      cartX[i] = cartX[i] + inc;
+      inc++;
+    }
+  }
+  inc = 0;
+  for (let i = 0; i < cartY.length; i++) {
+    if (cartY[i] === universeExpansion) {
+      inc += universeExpansion;
+      cartY[i] = inc;
+    } else {
+      cartY[i] = cartY[i] + inc;
+      inc++;
+    }
+  }
 
-const extractG = (input) => {
-  
+  for (let i = 0; i < galaxyPositions.length; i++) {
+    for (let j = i + 1; j < galaxyPositions.length; j++) {
+      const G1X = cartX[galaxyPositions[i] % universeWidth];
+      const G1Y = cartY[Math.floor(galaxyPositions[i] / universeWidth)];
+      const G2X = cartX[galaxyPositions[j] % universeWidth];
+      const G2Y = cartY[Math.floor(galaxyPositions[j] / universeWidth)];
+
+      const XDiff = Math.abs(G1X - G2X);
+      const YDiff = Math.abs(G1Y - G2Y);
+
+      sum += XDiff + YDiff;
+    }
+  }
+  return sum;
 };
 
 const readFile = (filename, callback) => {
@@ -31,7 +64,7 @@ const readFile = (filename, callback) => {
 const runS = (filename) => {
   const callback = (data) => {
     console.time();
-    console.log(`Response Silver ${filename}: ${extractS(data)}`);
+    console.log(`Response Silver ${filename}: ${extract(data, 2)}`);
     console.timeEnd();
   };
   readFile(filename, callback);
@@ -40,13 +73,13 @@ const runS = (filename) => {
 const runG = (filename) => {
   const callback = (data) => {
     console.time();
-    console.log(`Response Gold ${filename}: ${extractG(data)}`);
+    console.log(`Response Gold ${filename}: ${extract(data, 1000000)}`);
     console.timeEnd();
   };
   readFile(filename, callback);
 };
 
 runS("sample.txt");
-// runG("sample.txt");
-// runS("input.txt");
-// runG("input.txt");
+runG("sample.txt");
+runS("input.txt");
+runG("input.txt");
